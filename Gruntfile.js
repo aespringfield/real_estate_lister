24aee46646d68c4b8ef3ece16b4dfacf9f1d5b1f
Anna Springfield <anna.e.springfield@gmail.com>
2017-03-31 15:55:33 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,83 @@
+// Setup grunt and add the uglify module to the project
+module.exports = function(grunt) {
+  // Tell grunt how the project is configured
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // MUST LOAD & REGISTER UGLIFY PLUGIN (see below)
+    uglify: {
+      client: {
+        // src can be an array [] or wildcard '*.js'
+        src: 'client/scripts/client.js',
+        dest: 'server/public/scripts/client.min.js'
+      }
+    },
+    // MUST LOAD & REGISTER COPY PLUGIN (see below)
+    copy: {
+      scripts: {
+        // Required but not getting into at this point
+        expand: true,
+        // Current working directory, where are the files?
+        cwd: 'client/scripts/',
+        // List of files to copy
+        src: ['*.*'], // Could use ['*.html'] or ['*.*']
+        // Destination, where should we put them?
+        dest: 'server/public/scripts/'
+      },
+      views: {
+        // Required but not getting into at this point
+        expand: true,
+        // Current working directory, where are the files?
+        cwd: 'client/views/',
+        // List of files to copy
+        src: ['*.*'], // Could use ['*.html'] or ['*.*']
+        // Destination, where should we put them?
+        dest: 'server/public/views/'
+      },
+      // name of the task e.g. jquery
+      jquery: {
+        // Required but not getting into at this point
+        expand: true,
+        // Current working directory, where are the files?
+        cwd: 'node_modules/jquery/dist/',
+        // List of files to copy
+        src: ['jquery.js'],
+        // Destination, where should we put them?
+        dest: 'server/public/vendors/'
+      },
+      bootstrap_css: {
+        // Required but not getting into at this point
+        expand: true,
+        // Current working directory, where are the files?
+        cwd: 'node_modules/bootstrap/dist/css',
+        // List of files to copy
+        src: ['bootstrap.css'], // Could use ['*.html'] or ['*.*']
+        // Destination, where should we put them?
+        dest: 'server/public/vendors/bootstrap'
+      },
+      bootstrap_js: {
+        // Required but not getting into at this point
+        expand: true,
+        // Current working directory, where are the files?
+        cwd: 'node_modules/bootstrap/dist/js',
+        // List of files to copy
+        src: ['bootstrap.js'], // Could use ['*.html'] or ['*.*']
+        // Destination, where should we put them?
+        dest: 'server/public/vendors/bootstrap'
+      },
+    },
+    // No sub tasks for watch
+    watch: {
+      // What files am I looking at
+      files: ['client/scripts/*.js', 'client/views/*.html'],
+      // What tasks should I complete
+      tasks: ['uglify', 'copy']
+    }
+  });
+  // LOAD PLUGIN: Bring the plugin into the project
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // REGISTER TASK: Actually use the plugin
+  grunt.registerTask('default', [/*'uglify',*/'copy','watch']);
+};
---
+++ listingData.js
@@ -0,0 +1,701 @@
+
+{
+	"_id" : ObjectId("58de9673d0c1cb22fbcb2655"),
+	"cost" : 123000,
+	"sqft" : 1500,
+	"city" : "Forest Lake",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9674d0c1cb22fbcb2656"),
+	"rent" : 18000,
+	"sqft" : 600,
+	"city" : "Victoria",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9678d0c1cb22fbcb2657"),
+	"cost" : 40800,
+	"sqft" : 400,
+	"city" : "Orono",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9678d0c1cb22fbcb2658"),
+	"rent" : 11400,
+	"sqft" : 600,
+	"city" : "Mahtomedi",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9678d0c1cb22fbcb2659"),
+	"rent" : 31500,
+	"sqft" : 2100,
+	"city" : "St. Joseph",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9679d0c1cb22fbcb265a"),
+	"rent" : 13500,
+	"sqft" : 900,
+	"city" : "Mahtomedi",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9679d0c1cb22fbcb265b"),
+	"rent" : 3600,
+	"sqft" : 300,
+	"city" : "Oak Park Heights",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9679d0c1cb22fbcb265c"),
+	"cost" : 32500,
+	"sqft" : 500,
+	"city" : "Blaine",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9679d0c1cb22fbcb265d"),
+	"cost" : 127500,
+	"sqft" : 1500,
+	"city" : "Oakdale",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ad0c1cb22fbcb265e"),
+	"rent" : 52200,
+	"sqft" : 1800,
+	"city" : "Bemidji",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ad0c1cb22fbcb265f"),
+	"cost" : 126100,
+	"sqft" : 1300,
+	"city" : "Rochester",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ad0c1cb22fbcb2660"),
+	"rent" : 18900,
+	"sqft" : 700,
+	"city" : "Waconia",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967bd0c1cb22fbcb2661"),
+	"cost" : 105300,
+	"sqft" : 900,
+	"city" : "Zimmerman",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967bd0c1cb22fbcb2662"),
+	"rent" : 18700,
+	"sqft" : 1700,
+	"city" : "Falcon Heights",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967bd0c1cb22fbcb2663"),
+	"rent" : 30600,
+	"sqft" : 1800,
+	"city" : "Hutchinson",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967bd0c1cb22fbcb2664"),
+	"rent" : 31200,
+	"sqft" : 1200,
+	"city" : "Champlin",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967cd0c1cb22fbcb2665"),
+	"cost" : 80300,
+	"sqft" : 1100,
+	"city" : "Hopkins",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967cd0c1cb22fbcb2666"),
+	"cost" : 148800,
+	"sqft" : 1600,
+	"city" : "International Falls",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967dd0c1cb22fbcb2667"),
+	"cost" : 35700,
+	"sqft" : 300,
+	"city" : "Circle Pines",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ed0c1cb22fbcb2668"),
+	"cost" : 51000,
+	"sqft" : 500,
+	"city" : "Grand Rapids",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ed0c1cb22fbcb2669"),
+	"rent" : 10800,
+	"sqft" : 900,
+	"city" : "Mound",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ed0c1cb22fbcb266a"),
+	"rent" : 14300,
+	"sqft" : 1300,
+	"city" : "White Bear Lake",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967ed0c1cb22fbcb266b"),
+	"rent" : 52800,
+	"sqft" : 2200,
+	"city" : "New Brighton",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967fd0c1cb22fbcb266c"),
+	"cost" : 45900,
+	"sqft" : 900,
+	"city" : "Fergus Falls",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967fd0c1cb22fbcb266d"),
+	"cost" : 159600,
+	"sqft" : 1900,
+	"city" : "Lake City",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967fd0c1cb22fbcb266e"),
+	"cost" : 96300,
+	"sqft" : 900,
+	"city" : "Hibbing",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de967fd0c1cb22fbcb266f"),
+	"rent" : 6000,
+	"sqft" : 600,
+	"city" : "Mound",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9680d0c1cb22fbcb2670"),
+	"rent" : 9600,
+	"sqft" : 800,
+	"city" : "Dayton",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9680d0c1cb22fbcb2671"),
+	"rent" : 12800,
+	"sqft" : 800,
+	"city" : "Kasson",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9680d0c1cb22fbcb2672"),
+	"rent" : 22500,
+	"sqft" : 900,
+	"city" : "Anoka",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9680d0c1cb22fbcb2673"),
+	"cost" : 139400,
+	"sqft" : 1700,
+	"city" : "Savage",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9681d0c1cb22fbcb2674"),
+	"cost" : 106400,
+	"sqft" : 1400,
+	"city" : "Edina",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9681d0c1cb22fbcb2675"),
+	"cost" : 134400,
+	"sqft" : 2400,
+	"city" : "North St. Paul",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9681d0c1cb22fbcb2676"),
+	"cost" : 248400,
+	"sqft" : 2300,
+	"city" : "Oakdale",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9681d0c1cb22fbcb2677"),
+	"rent" : 29900,
+	"sqft" : 1300,
+	"city" : "Maplewood",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9681d0c1cb22fbcb2678"),
+	"rent" : 32200,
+	"sqft" : 2300,
+	"city" : "Mounds View",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9682d0c1cb22fbcb2679"),
+	"rent" : 16100,
+	"sqft" : 700,
+	"city" : "Savage",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9682d0c1cb22fbcb267a"),
+	"rent" : 3300,
+	"sqft" : 300,
+	"city" : "Marshall",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9682d0c1cb22fbcb267b"),
+	"cost" : 88400,
+	"sqft" : 1700,
+	"city" : "Albertville",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9683d0c1cb22fbcb267c"),
+	"rent" : 36000,
+	"sqft" : 1500,
+	"city" : "Robbinsdale",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9683d0c1cb22fbcb267d"),
+	"rent" : 50400,
+	"sqft" : 2400,
+	"city" : "Kasson",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9683d0c1cb22fbcb267e"),
+	"rent" : 19000,
+	"sqft" : 1000,
+	"city" : "Lake City",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9683d0c1cb22fbcb267f"),
+	"rent" : 15600,
+	"sqft" : 1200,
+	"city" : "Champlin",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9683d0c1cb22fbcb2680"),
+	"cost" : 148200,
+	"sqft" : 1300,
+	"city" : "Minneapolis",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9683d0c1cb22fbcb2681"),
+	"cost" : 54000,
+	"sqft" : 1000,
+	"city" : "East Grand Forks",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9684d0c1cb22fbcb2682"),
+	"rent" : 28000,
+	"sqft" : 1400,
+	"city" : "Shakopee",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9684d0c1cb22fbcb2683"),
+	"rent" : 28800,
+	"sqft" : 1800,
+	"city" : "Isanti",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9684d0c1cb22fbcb2684"),
+	"cost" : 165600,
+	"sqft" : 2400,
+	"city" : "Oakdale",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9685d0c1cb22fbcb2685"),
+	"rent" : 10400,
+	"sqft" : 400,
+	"city" : "Columbia Heights",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9685d0c1cb22fbcb2686"),
+	"cost" : 36000,
+	"sqft" : 800,
+	"city" : "Waseca",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9685d0c1cb22fbcb2687"),
+	"cost" : 106400,
+	"sqft" : 1900,
+	"city" : "North Oaks",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9686d0c1cb22fbcb2688"),
+	"rent" : 64800,
+	"sqft" : 2400,
+	"city" : "Little Falls",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9686d0c1cb22fbcb2689"),
+	"rent" : 18000,
+	"sqft" : 1200,
+	"city" : "Champlin",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9686d0c1cb22fbcb268a"),
+	"cost" : 93600,
+	"sqft" : 800,
+	"city" : "Inver Grove Heights",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968cd0c1cb22fbcb268b"),
+	"cost" : 90200,
+	"sqft" : 2200,
+	"city" : "Minnetonka",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968cd0c1cb22fbcb268c"),
+	"cost" : 97500,
+	"sqft" : 1300,
+	"city" : "Andover",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968cd0c1cb22fbcb268d"),
+	"rent" : 30000,
+	"sqft" : 1000,
+	"city" : "Ham Lake",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968cd0c1cb22fbcb268e"),
+	"rent" : 8100,
+	"sqft" : 300,
+	"city" : "St. Joseph",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968dd0c1cb22fbcb268f"),
+	"cost" : 59000,
+	"sqft" : 500,
+	"city" : "North Mankato",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968dd0c1cb22fbcb2690"),
+	"rent" : 32200,
+	"sqft" : 1400,
+	"city" : "International Falls",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968dd0c1cb22fbcb2691"),
+	"rent" : 52200,
+	"sqft" : 1800,
+	"city" : "Mendota Heights",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968ed0c1cb22fbcb2692"),
+	"cost" : 100300,
+	"sqft" : 1700,
+	"city" : "Shakopee",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968ed0c1cb22fbcb2693"),
+	"rent" : 12500,
+	"sqft" : 500,
+	"city" : "Otsego",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968ed0c1cb22fbcb2694"),
+	"cost" : 63000,
+	"sqft" : 600,
+	"city" : "Chisago City",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968fd0c1cb22fbcb2695"),
+	"cost" : 170100,
+	"sqft" : 2100,
+	"city" : "New Brighton",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968fd0c1cb22fbcb2696"),
+	"cost" : 68600,
+	"sqft" : 700,
+	"city" : "Glencoe",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de968fd0c1cb22fbcb2697"),
+	"rent" : 52000,
+	"sqft" : 2000,
+	"city" : "Plymouth",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9690d0c1cb22fbcb2698"),
+	"cost" : 33300,
+	"sqft" : 300,
+	"city" : "Brooklyn Park",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9690d0c1cb22fbcb2699"),
+	"cost" : 83200,
+	"sqft" : 1600,
+	"city" : "West St. Paul",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9690d0c1cb22fbcb269a"),
+	"rent" : 50400,
+	"sqft" : 2100,
+	"city" : "Mankato",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9690d0c1cb22fbcb269b"),
+	"rent" : 29400,
+	"sqft" : 2100,
+	"city" : "Hastings",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9691d0c1cb22fbcb269c"),
+	"cost" : 74100,
+	"sqft" : 1300,
+	"city" : "Columbia Heights",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9691d0c1cb22fbcb269d"),
+	"cost" : 169500,
+	"sqft" : 1500,
+	"city" : "Apple Valley",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9691d0c1cb22fbcb269e"),
+	"cost" : 63600,
+	"sqft" : 1200,
+	"city" : "Plymouth",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9691d0c1cb22fbcb269f"),
+	"rent" : 11000,
+	"sqft" : 500,
+	"city" : "Rosemount",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9691d0c1cb22fbcb26a0"),
+	"rent" : 25300,
+	"sqft" : 2300,
+	"city" : "Spring Lake Park",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9692d0c1cb22fbcb26a1"),
+	"cost" : 149600,
+	"sqft" : 2200,
+	"city" : "East Bethel",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9692d0c1cb22fbcb26a2"),
+	"cost" : 47600,
+	"sqft" : 400,
+	"city" : "Big Lake",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9692d0c1cb22fbcb26a3"),
+	"cost" : 24000,
+	"sqft" : 300,
+	"city" : "Wyoming",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9692d0c1cb22fbcb26a4"),
+	"rent" : 10500,
+	"sqft" : 500,
+	"city" : "Chaska",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9692d0c1cb22fbcb26a5"),
+	"rent" : 5100,
+	"sqft" : 300,
+	"city" : "Crystal",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9693d0c1cb22fbcb26a6"),
+	"cost" : 100800,
+	"sqft" : 1200,
+	"city" : "Burnsville",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9693d0c1cb22fbcb26a7"),
+	"cost" : 116000,
+	"sqft" : 1000,
+	"city" : "Bemidji",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9693d0c1cb22fbcb26a8"),
+	"cost" : 51000,
+	"sqft" : 600,
+	"city" : "Little Canada",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9693d0c1cb22fbcb26a9"),
+	"cost" : 80300,
+	"sqft" : 1100,
+	"city" : "Shorewood",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9694d0c1cb22fbcb26aa"),
+	"rent" : 20000,
+	"sqft" : 1000,
+	"city" : "Delano",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9694d0c1cb22fbcb26ab"),
+	"rent" : 20400,
+	"sqft" : 1700,
+	"city" : "Redwood Falls",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9694d0c1cb22fbcb26ac"),
+	"rent" : 26600,
+	"sqft" : 1900,
+	"city" : "Minnetrista",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9694d0c1cb22fbcb26ad"),
+	"rent" : 25200,
+	"sqft" : 1800,
+	"city" : "Rosemount",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9695d0c1cb22fbcb26ae"),
+	"cost" : 139100,
+	"sqft" : 1300,
+	"city" : "St. Anthony",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de9695d0c1cb22fbcb26af"),
+	"cost" : 259200,
+	"sqft" : 2400,
+	"city" : "Minneapolis",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a4d0c1cb22fbcb26b0"),
+	"rent" : 6400,
+	"sqft" : 400,
+	"city" : "Shakopee",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a5d0c1cb22fbcb26b1"),
+	"cost" : 234600,
+	"sqft" : 2300,
+	"city" : "Stewartville",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a5d0c1cb22fbcb26b2"),
+	"rent" : 29900,
+	"sqft" : 1300,
+	"city" : "Rochester",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a6d0c1cb22fbcb26b3"),
+	"rent" : 33600,
+	"sqft" : 2100,
+	"city" : "Waite Park",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a6d0c1cb22fbcb26b4"),
+	"cost" : 85200,
+	"sqft" : 1200,
+	"city" : "Duluth",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a6d0c1cb22fbcb26b5"),
+	"cost" : 178500,
+	"sqft" : 1500,
+	"city" : "St. Joseph",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a7d0c1cb22fbcb26b6"),
+	"rent" : 22000,
+	"sqft" : 1100,
+	"city" : "Virginia",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a7d0c1cb22fbcb26b7"),
+	"cost" : 129800,
+	"sqft" : 2200,
+	"city" : "West St. Paul",
+	"__v" : 0
+}
+{
+	"_id" : ObjectId("58de96a7d0c1cb22fbcb26b8"),
+	"rent" : 45900,
+	"sqft" : 1700,
+	"city" : "International Falls",
+	"__v" : 0
+}
